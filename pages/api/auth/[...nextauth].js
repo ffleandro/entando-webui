4264ac44e086b296d1ebf9f45ba08c9353b89d43
Filipe Leandro <filipeleandro87@gmail.com>
2021-04-19 20:23:48 -0300

    Base web ui pages and support for legacy portal ui proxying

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,46 @@
+import NextAuth from 'next-auth';
+
+import { keycloakCfg } from '../../_keycloak.tsx';
+
+export default NextAuth({
+  providers: [
+    {
+      id: 'entando',
+      name: 'Entando',
+      type: 'oauth',
+      version: '2.0',
+      scope: 'profile openid email',
+      params: { grant_type: 'authorization_code' },
+      accessTokenUrl: `${keycloakCfg.url}/realms/${keycloakCfg.realm}/protocol/openid-connect/token`,
+      authorizationUrl: `${keycloakCfg.url}/realms/${keycloakCfg.realm}/protocol/openid-connect/auth?response_type=code`,
+      profileUrl: `${keycloakCfg.url}/realms/${keycloakCfg.realm}/protocol/openid-connect/userinfo`,
+      async profile(profile) {
+        return {
+          id: profile.preferred_username,
+          preferred_username: profile.preferred_username,
+          name: '',
+          email: profile.preferred_username,
+          image: '',
+        };
+      },
+      clientId: 'entando-web',
+      callbacks: {
+        async session(session, user) {
+          console.log('----------> Add Props to Session!', user);
+          return session;
+        },
+        async redirect(url, baseUrl) {
+          console.log('----------> Validating Redirect!', url);
+          return url;
+        },
+      },
+    },
+  ],
+  debug: process.env.WEBUI_DEBUG || false,
+  secret: process.env.APP_SECRET,
+  session: {
+    // More information at: https://next-auth.js.org/configuration/options#session
+    jwt: true,
+    maxAge: 24 * 60 * 60, //24 hours
+  },
+});
