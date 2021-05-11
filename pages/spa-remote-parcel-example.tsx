9f8d868ba9c8b882c32af813fe2f993d62f51156
Filipe Leandro <filipeleandro87@gmail.com>
2021-05-11 17:43:22 -0300

    Removed absolute MFE urls and fixed identation

--- pages/spa-remote-parcel-example.tsx
+++ pages/spa-remote-parcel-example.tsx
@@ -1,35 +1,36 @@
 import { NextPage } from 'next';
-import {SingleSpaLayout} from "../components/SingleSpaLayout";
-import SSPAWidget, {createImportMapFragment} from "../components/SSPAWidget";
+
+import { SingleSpaLayout } from '../components/SingleSpaLayout';
+import SSPAWidget, { createImportMapFragment } from '../components/SSPAWidget';
 
 interface Props {}
 
-const importMaps=`{
+const importMaps = `{
     "imports": {
     "react": "https://cdn.jsdelivr.net/npm/react@17.0.1/umd/react.production.min.js",
     "react-dom": "https://cdn.jsdelivr.net/npm/react-dom@17.0.1/umd/react-dom.production.min.js",
     "single-spa": "https://cdn.jsdelivr.net/npm/single-spa/lib/system/single-spa.dev.js"
-    }                        }`
+    }                        }`;
 
-const mfApp2 ={
-  mfName: "@entando/app2",
-  mfURL: "http://localhost:3000/entando-app2.js",
+const mfApp2 = {
+  mfName: '@entando/app2',
+  mfURL: '/entando-app2.js',
   customProps: {
-    "echo": "this is an echo"
+    echo: 'this is an echo',
   },
-}
-
+};
 
 const SpaRemoteParcelExamplePage: NextPage<Props> = () => {
   return (
-      <SingleSpaLayout>
-        <script type="systemjs-importmap" dangerouslySetInnerHTML={{ __html: importMaps }}>
-        </script>
-        <script src="https://cdn.jsdelivr.net/npm/systemjs/dist/system.js"></script>
-        <script type="systemjs-importmap" dangerouslySetInnerHTML={{ __html: createImportMapFragment({...mfApp2}) }}></script>
-        <SSPAWidget {...mfApp2}/>
-
-      </SingleSpaLayout>
+    <SingleSpaLayout>
+      <script type="systemjs-importmap" dangerouslySetInnerHTML={{ __html: importMaps }}></script>
+      <script src="https://cdn.jsdelivr.net/npm/systemjs/dist/system.js"></script>
+      <script
+        type="systemjs-importmap"
+        dangerouslySetInnerHTML={{ __html: createImportMapFragment({ ...mfApp2 }) }}
+      ></script>
+      <SSPAWidget {...mfApp2} />
+    </SingleSpaLayout>
   );
 };
 
